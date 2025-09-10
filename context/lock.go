2fff96f1b6a5835064122485a6765810e0f0146a
cloudimpl <github.bot@cloudimpl.com>
2025-09-10 04:55:20 +0000

    Update from https://github.com/cloudimpl/ByteOS/commit/aeadb6523c17213430b04408badc2dd45f75c9b8

--- context/lock.go
+++ context/lock.go
@@ -1,8 +1,31 @@
 package context
 
-import "time"
+import (
+	"github.com/cloudimpl/byte-os/runtime"
+	"time"
+)
 
-type Lock interface {
-	Acquire(expireIn time.Duration) error
-	Release() error
+type Lock struct {
+	client    runtime.ServiceClient
+	sessionId string
+	key       string
+}
+
+func (l *Lock) Acquire(expireIn time.Duration) error {
+	ttl := time.Now().Unix() + int64(expireIn.Seconds())
+
+	req := runtime.AcquireLockRequest{
+		Key: l.key,
+		TTL: ttl,
+	}
+
+	return l.client.AcquireLock(l.sessionId, req)
+}
+
+func (l *Lock) Release() error {
+	req := runtime.ReleaseLockRequest{
+		Key: l.key,
+	}
+
+	return l.client.ReleaseLock(l.sessionId, req)
 }
--- context/logger.go
+++ context/logger.go
@@ -1,17 +1,104 @@
 package context
 
-type LogEntry interface {
-	Str(key string, val string) LogEntry
-	Int64(key string, val int64) LogEntry
-	Float64(key string, val float64) LogEntry
-	Bool(key string, val bool) LogEntry
-	Done()
-	Msg(msg string)
+import (
+	"encoding/json"
+	"time"
+)
+
+type LogLevel string
+
+const (
+	DebugLevel LogLevel = "DEBUG"
+	InfoLevel  LogLevel = "INFO"
+	WarnLevel  LogLevel = "WARN"
+	ErrorLevel LogLevel = "ERROR"
+)
+
+type LogMsg struct {
+	Level     LogLevel               `json:"level"`
+	Section   string                 `json:"section"`
+	Tags      map[string]interface{} `json:"tags"`
+	Timestamp int64                  `json:"timestamp"`
+	Message   string                 `json:"message"`
+}
+
+type LogEntry struct {
+	msg *LogMsg
+}
+
+func (entry *LogEntry) Str(key string, val string) *LogEntry {
+	entry.msg.Tags[key] = val
+	return entry
+}
+
+func (entry *LogEntry) Int64(key string, val int64) *LogEntry {
+	entry.msg.Tags[key] = val
+	return entry
+}
+
+func (entry *LogEntry) Float64(key string, val float64) *LogEntry {
+	entry.msg.Tags[key] = val
+	return entry
+}
+
+func (entry *LogEntry) Bool(key string, val bool) *LogEntry {
+	entry.msg.Tags[key] = val
+	return entry
+}
+
+func (entry *LogEntry) Done() {
+	entry.msg.Timestamp = time.Now().UnixMicro()
+	logJson, err := json.Marshal(entry.msg)
+	if err == nil {
+		println(string(logJson))
+	}
+}
+
+func (entry *LogEntry) Msg(msg string) {
+	entry.msg.Message = msg
+	entry.Done()
 }
 
 type Logger interface {
-	Debug() LogEntry
-	Info() LogEntry
-	Warn() LogEntry
-	Error() LogEntry
+	Log(level LogLevel) *LogEntry
+	Debug() *LogEntry
+	Info() *LogEntry
+	Warn() *LogEntry
+	Error() *LogEntry
+}
+
+type JsonLogger struct {
+	section string
+}
+
+func (logger *JsonLogger) Log(level LogLevel) *LogEntry {
+	return &LogEntry{
+		msg: &LogMsg{
+			Level:   level,
+			Section: logger.section,
+			Tags:    make(map[string]interface{}),
+		},
+	}
+}
+
+func (logger *JsonLogger) Debug() *LogEntry {
+	return logger.Log(DebugLevel)
+}
+
+func (logger *JsonLogger) Info() *LogEntry {
+	return logger.Log(InfoLevel)
+}
+
+func (logger *JsonLogger) Warn() *LogEntry {
+	return logger.Log(WarnLevel)
+}
+
+func (logger *JsonLogger) Error() *LogEntry {
+	return logger.Log(ErrorLevel)
+}
+
+func CreateLogger(section string) Logger {
+	return &JsonLogger{
+		section: section,
+	}
 }
